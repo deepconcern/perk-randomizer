705db3c53856858eab0f51e7afa399cf3598243c
Wyatt Barnes <wyattlbarnes@gmail.com>
2024-10-01 22:15:00 -0700

    Testing version bump

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,9 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App.tsx";
 
 // Nuke the local storage on version bump
-const storageVersion = localStorage.getItem("storage-version");
 
-if (storageVersion !== import.meta.env.VITE_STORAGE_VERSION) {
-  for (let keyIndex = 0, length = localStorage.length; keyIndex < length; keyIndex++) {
-    const key = localStorage.key(keyIndex);
-
-    if (!key) continue;
-
-    localStorage.removeItem(key);
-  }
+if (localStorage.getItem("storage-version") !== import.meta.env.VITE_STORAGE_VERSION) {
+  localStorage.clear();
 
   localStorage.setItem("storage-version", import.meta.env.VITE_STORAGE_VERSION);
 }
